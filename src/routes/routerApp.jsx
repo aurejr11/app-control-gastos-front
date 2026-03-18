c73327de04f4f7f3e010a012147d7000340265f1
VELASQUEZ <aurejr11@gmail.com>
2026-03-18 08:52:51 -0500

    (chore): Instalacion y configuracion parcial del react-router-dp, + instalacion de sweetalert2

---
+++ src/routes/routerApp.jsx
@@ -0,0 +1,9 @@
+
+import Login from '../pages/Login'  // siempre debe estar importado el componente que queremos mostrars
+let routerApp = [
+    {
+        path:"/",
+        element: <Login />  // llamar a la primera visual que queremos mostrar al iniciar la app o pagina web
+
+    }
+]
